0dc715f2207e72b3ade2e6db17eb186e80a3b094
Arpon das <arpon.front@gmail.com>
2026-03-29 10:37:28 +0600

    status: second commite with bug

---
+++ app/lib/navData.ts
@@ -0,0 +1,13 @@
+// navData.js
+export const visitorLinks = [
+  { name: 'Home', path: '/' },
+  { name: 'About Us', path: '/about' },
+  { name: 'Services', path: '/services' },
+];
+
+export const userLinks = [
+  { name: 'Home', path: '/' },
+  { name: 'Dashboard', path: '/dashboard' },
+  { name: 'My Bookings', path: '/bookings' },
+  { name: 'Settings', path: '/settings' },
+];
